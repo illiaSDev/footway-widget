46c55bc25da4acdee08a986629ba08aa573e4688
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2018-06-15 08:06:54 +0300

    enabled rotation again

--- globe/globe.js
+++ globe/globe.js
@@ -77,7 +77,7 @@ DAT.Globe = function(container, opts) {
   var zoomSpeed = 50;
 
   var mouse = { x: 0, y: 0 }, mouseOnDown = { x: 0, y: 0 };
-  var rotation = { x: 0, y: 0 }, incr_rotation = { x: 0, y: 0 }
+  var rotation = { x: 0, y: 0 }, incr_rotation = { x: 0.001, y: 0 }
       target = { x: Math.PI*3/2, y: Math.PI / 6.0 },
       targetOnDown = { x: 0, y: 0 };
 
@@ -355,8 +355,8 @@ DAT.Globe = function(container, opts) {
 
     target.x += incr_rotation.x;
     target.y += incr_rotation.y;
-    target.x = 4.9;
-    target.y = 0.9;
+    // target.x = 4.9;
+    // target.y = 0.9;
 
     rotation.x += (target.x - rotation.x) * 0.1;
     rotation.y += (target.y - rotation.y) * 0.1;
